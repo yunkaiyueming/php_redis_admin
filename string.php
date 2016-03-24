898cb2f8a68dc3b6c5c131f87c1276e06783445a
hongcheng.xu <819664855@qq.com>
2016-03-24 10:35:11 +0800

    补充string操作

--- string.php
+++ string.php
@@ -12,7 +12,7 @@ $rand_val = rand(1, 100);
 //$redis->decr("test_num");
 //echo $redis->get("test_num");
 
-//$redis->append("test_num", "_appid1000");
+//$redis->append("test_num", "_appid1000");//将value追加到 key 原来的值的末尾。
 //echo $redis->get("test_num");
 
 //echo $redis->getRange("test_num", 0, 4);
@@ -39,15 +39,30 @@ $rand_val = rand(1, 100);
 
 //$redis->expire("page_view", 1);设置过期时间
 
-// $exist_keys = $redis->keys("*");
+// $exist_keys = $redis->keys("*");//正则获取key
 // print_r($exist_keys);
 
 // $all_key_values = $redis->mget($exist_keys);
 // print_r($all_key_values);
 
-$mxi_arr = array('day'=>'100', 'weather'=>'cold', 'time'=>time());
-echo $redis->mset($mxi_arr);
+// $mxi_arr = array('day'=>'100', 'weather'=>'cold', 'time'=>time());
+// echo $redis->mset($mxi_arr);
 
-$mix_vals = $redis->mget(array_keys($mxi_arr));
-print_r($mix_vals);
+// $mix_vals = $redis->mget(array_keys($mxi_arr));//同时获取多个值
+// print_r($mix_vals);
+
+// $msetnx_res = $redis->msetnx(array('day'=>200, 'language'=>'english'));//同时设置多个值
+// var_dump($msetnx_res);
+
+//$redis->setex('name',10, "abc");//以秒为单位设置 key 的生存时间
+// echo $redis->pttl("name");
+
+//$redis->psetex("name2", 100, "twd");//以毫秒为单位设置 key 的生存时间
+// echo $redis->ttl("name2");
+
+//echo $redis->strlen("test_num");
+
+echo $redis->get("test_num");
+$redis->setRange("test_num", 2, "oookkk");
+echo $redis->get("test_num");
 ?>
